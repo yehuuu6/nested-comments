b38fce730f0c8bd945a1d15dd62f53d9e269af40
Eren Aydın <49097712+yehuuu6@users.noreply.github.com>
2025-10-31 23:06:53 +0300

    Install packages & set up project

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=inter:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
+        <title>{{ $title ?? 'Page Title' }}</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+
+    <body>
+        {{ $slot }}
+    </body>
+
+</html>
